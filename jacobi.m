65404092b2892346adb080a57a916c2a08e7185a
ebola <wchung.huang@gmail.com>
2025-01-30 09:42:53 +0800

    update

--- jacobi.m
+++ jacobi.m
@@ -1,12 +1,15 @@
-function [x,step] = jacobi(A,b,err)
+function [x,step] = jacobi(A,b,err,x0)
     n = size(A,1);
     H = diag(ones(n,1)./diag(A));
     B = eye(n) - H*A;
     g = H*b;
-    x0 = zeros(n,1);
-    x = g;
+    if nargin < 4
+        x = zeros(n,1);
+    else
+        x = x0;
+    end
     step = 0;
-    while vecnorm(x-x0)>err
+    while step == 0 || vecnorm(x-x0)>err
         x0 = x;
         x = B*x + g;
         step = step + 1;
