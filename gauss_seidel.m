502973dd6944a2ca8c19c0e12f364bb700f35ea6
ebola <wchung.huang@gmail.com>
2025-01-31 20:41:10 +0800

    update

--- gauss_seidel.m
+++ gauss_seidel.m
@@ -1,20 +1,18 @@
-function [x,step] = gauss_seidel(A,b,err)
-    n = size(A,1);
-    x0 = zeros(n,1);
-    x = b;
-    step = 0;
-    while vecnorm(x-x0)>err
-        x0 = x;
+function x = gauss_seidel(A, b, x0, tol, maxIter)
+    n = length(b);
+    x = x0;
+    for iter = 1:maxIter
+        x_old = x;
         for i = 1:n
-            x(i) = b(i);
-            for j = 1:n
-                if j~=i
-                    x(i) = x(i) - A(i,j)*x(j);
-                end
-            end
-            x(i) = x(i)/A(i,i);
+            s1 = A(i,1:i-1) * x(1:i-1); % 左侧部分求和
+            s2 = A(i,i+1:n) * x_old(i+1:n); % 右侧部分求和（使用旧解）
+            x(i) = (b(i) - s1 - s2) / A(i,i); % 更新当前解
         end
-        step = step + 1;
+        
+        if max(abs(x - x_old)) <= tol
+            fprintf('Converged in %d iterations.\n', iter);
+            return;
+        end
     end
+    fprintf('Reach the maximum iteration times.\n');
 end
-
