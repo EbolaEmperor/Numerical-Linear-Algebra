515e8b467a2a7df28bd54ac4143c1be2b54acb58
ebola <wchung.huang@gmail.com>
2025-03-13 21:51:27 +0800

    accelerated

--- choleskyImproved.m
+++ choleskyImproved.m
@@ -1,11 +1,10 @@
 function [L, D] = choleskyImproved(A)
 %   改进的cholesky分解算法，计算L和D使得A=LDL^T
-    n = size(A,1);
+    n = size(A, 1);
     L = eye(n);
-    D = zeros(1,n);
+    D = zeros(1, n);
     for j = 1:n
-        v = D(1:j-1).*L(j,1:j-1);
-        D(j) = A(j,j) - dot(L(j,1:j-1),v);
-        L(j+1:n,j) = (A(j+1:n,j)-L(j+1:n,1:j-1)*(v.'))/D(j);
+        D(j) = A(j, j) - L(j, 1:j-1).^2 * D(1:j-1)';
+        L(j+1:n,j) = (A(j+1:n, j) - L(j+1:n, 1:j-1) * (L(j, 1:j-1) .* D(1:j-1))') / D(j);
     end
 end
--- solveLowerTriangularEquation.m
+++ solveLowerTriangularEquation.m
@@ -1,9 +1,9 @@
 function x = solveLowerTriangularEquation(L, b)
-    n = size(L,1);
-    for i = 1:n-1
-        b(i,1) = b(i,1)/L(i,i);
-        b(i+1:n,1) = b(i+1:n,1) - b(i)*L(i+1:n,i);
+    n = size(L, 1);
+    for i = 1 : n-1
+        b(i) = b(i) / L(i,i);
+        b(i+1:n) = b(i+1:n) - b(i) * L(i+1:n, i);
     end
-    b(n,1) = b(n,1)/L(n,n);
+    b(n) = b(n) / L(n, n);
     x = b;
 end
